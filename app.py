5ca62bfbd1a0d72d3efafde9bfdb6e20e2ab4ef0
myname21-meet <myname21@meet.mit.edu>
2019-07-30 11:29:42 -0400

    tampels

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home_page():
-    return "Hello World"
+def hello_world():
+    return render_template("index.html", name= "katerina", food_list= ["strawberry",'chocolate',"spageti"], age=66)
+
+@app.route('/info/<string:info_name>/<int:age>')
+def profile(info_name,age):
+	return render_template("index.html", name=info_name, food_list=[], age=age)
 
 if __name__ == '__main__':
-   app.run(debug = True)
+    app.run(debug = True)
